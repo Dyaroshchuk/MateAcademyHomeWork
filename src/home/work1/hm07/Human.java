ae0e04fdf480cb73c02d5684c3e172df359bc6bc
Dyaroshchuk <ydawork1@gmail.com>
2019-03-24 00:49:16 +0200

    hm_1.6

---
+++ src/home/work1/hm07/Human.java
@@ -0,0 +1,106 @@
+package home.work1.hm07;
+
+public class Human implements Cloneable {
+    private String name;
+    private int age;
+    private Father father;
+
+    public String getFather() {
+        return father.getName();
+    }
+
+    public void setFather(String n) {
+        father.setName(n);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Human(String name, int age, String father) {
+        this.name = name;
+        this.age = age;
+        this.father = new Father(father);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + getAge();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Human other = (Human) obj;
+        if (getName() != other.getName()) {
+            return false;
+        }
+        if (getAge() != other.getAge()) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + " Age:" + getAge() + " HashCode:"
+                        + hashCode() + "Father: " + father.getName();
+    }
+
+    @Override
+    protected Human clone() throws CloneNotSupportedException {
+        Human newHuman = (Human) super.clone();
+        newHuman.father = (Father) father.clone();
+        return newHuman;
+    }
+
+    class Father implements Cloneable {
+
+        private String name;
+
+        public void setName(String n) {
+            name = n;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Father(String name) {
+            this.name = name;
+        }
+
+        @Override
+        protected Father clone() throws CloneNotSupportedException {
+            return (Father) super.clone();
+        }
+    }
+}
---
+++ src/home/work1/hm07/ObjectMethods.java
@@ -0,0 +1,38 @@
+package home.work1.hm07;
+
+/**
+ * 1.7 Реализовать каждый из Object методов, в частности - метод clone().
+ * 1.7 clone (deep clone)
+ */
+
+public class ObjectMethods implements Cloneable {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        Human eliot = new Human("Eliot", 25, "John");
+        Human secondEliot = new Human("Eliot", 25, "John");
+
+
+        System.out.println(eliot.equals(secondEliot)); //method equals
+        System.out.println(eliot.hashCode()); //method hashCode
+        System.out.println(secondEliot.toString()); //toString
+        System.out.println(eliot.getClass()); // getCalss
+        Human thirdElior = (Human) eliot.clone(); //deep clone eliot
+        System.out.println();
+
+        System.out.println(eliot); //information eliot
+        System.out.println(thirdElior); //information eliot clone
+
+        thirdElior.setFather("Gogi"); //change eliot clone Father
+        System.out.println();
+
+        System.out.println(eliot); //information eliot
+        System.out.println(thirdElior); //information eliot clone
+
+
+
+
+
+
+    }
+}
