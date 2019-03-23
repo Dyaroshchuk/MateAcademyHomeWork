059cac0facc78d64bfd7bf910c6f88237e654124
Dyaroshchuk <ydawork1@gmail.com>
2019-03-24 00:48:30 +0200

    hm_1.3

---
+++ src/home/work1/hm03/Power.java
@@ -0,0 +1,28 @@
+package home.work1.hm03;
+
+/**
+ * 1.3 Возвести число в квадрат, куб, степень. Важно: не использовать Math.pow().
+ */
+
+public class Power {
+
+    public static int myPow(int a, int pow) {
+        if (pow == 0) {
+            return 1;
+        } else {
+        int result = a;
+        for (int i = 1; i < pow; i++) {
+            result *= a;
+        }
+        return result;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(myPow(12, 2)); //check pow result 144
+
+    }
+
+
+}
