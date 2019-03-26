b8f16cb4c38bb0f3d88bcf563a10236ed5b74d3a
Dyaroshchuk <ydawork1@gmail.com>
2019-03-26 11:43:11 +0200

    Rhombus without - ((

--- src/home/work1/hm02/Rhombus.java
+++ src/home/work1/hm02/Rhombus.java
@@ -21,7 +21,7 @@ public class Rhombus {
 
     public static void printRhombusSymbols(int row, int speces) {
         for (int j = 0; j <= speces; j++) {
-            System.out.print("-");
+            System.out.print(" ");
         }
         for (int j = 1; j <= row; j++) {
             System.out.print("* ");
