433b2ea07148766a3d151e8f9f4a3bfe8ae59e5e
Dyaroshchuk <ydawork1@gmail.com>
2019-03-28 11:46:43 +0200

    deleted ArrayMethods object

--- src/home/work3/hm2/Main.java
+++ src/home/work3/hm2/Main.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        ArraysMethods arraysMethods = new ArraysMethods();
-        int[] arrayOne = arraysMethods.fillArray(7);
-        int[] arrayTwo = arraysMethods.fillArray(7);
+        int[] arrayOne = ArraysMethods.fillArray(7);
+        int[] arrayTwo = ArraysMethods.fillArray(7);
 
         // sorting arrays
         Arrays.sort(arrayOne);
         Arrays.sort(arrayTwo);
 
         // merging arrays to one
-        int[] arrayMerged = arraysMethods.merge(arrayOne, arrayTwo);
+        int[] arrayMerged = ArraysMethods.merge(arrayOne, arrayTwo);
 
         System.out.println(Arrays.toString(arrayOne));
         System.out.println(Arrays.toString(arrayTwo));
