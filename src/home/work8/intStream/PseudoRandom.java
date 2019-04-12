9f86b4df1659f755ad4ed7914fca7f3ffd31af40
Dyaroshchuk <ydawork1@gmail.com>
2019-04-13 00:23:59 +0300

    pseudorandom check not 0

--- src/home/work8/intStream/PseudoRandom.java
+++ src/home/work8/intStream/PseudoRandom.java
@@ -5,6 +5,10 @@ import java.util.stream.IntStream;
 public class PseudoRandom {
 
     public static IntStream pseudoRandomStream(int seed) {
-        return IntStream.iterate(seed, n -> n * n / 10 % 1000); // your implementation here
+        if (seed < 0) {
+            throw new IllegalArgumentException("seed can't bee less than 0");
+        } else {
+            return IntStream.iterate(seed, n -> n * n / 10 % 1000);
+        }
     }
 }
